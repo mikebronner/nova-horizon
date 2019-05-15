1431605103385944b4f7ca75ff0df662a0272aa9
Mike Bronner <mike@genealabs.com>
2019-05-15 10:50:58 -0700

    Initial commit

---
+++ resources/js/tool.js
@@ -0,0 +1,9 @@
+Nova.booting((Vue, router, store) => {
+    router.addRoutes([
+        {
+            name: 'nova-horizon',
+            path: '/nova-horizon',
+            component: require('./components/Tool'),
+        },
+    ])
+})
